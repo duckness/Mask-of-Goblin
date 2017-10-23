0942383a4753009b0de0047e617f4d3885c4ca4b
Bjorn Lim <mail@bjornl.im>
2017-10-23 19:34:24 +0800

    Begin refactoring to use Vuex.

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+
+}
+
+const mutations = {
+
+}
+
+const getters = {
+
+}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations
+})
