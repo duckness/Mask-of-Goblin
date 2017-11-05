3c15736b261e972af215c3e30c39475822ea5ce2
Bjorn Lim <mail@bjornl.im>
2017-11-05 22:17:41 +0800

    Should have extra info for T5 skills as well.

--- src/components/hero/Description.vue
+++ src/components/hero/Description.vue
@@ -23,9 +23,19 @@
                 <label class="label no-margin-bottom">T5 Light</label>
                 <span>{{ hero.t5.light }}</span>
               </div>
+              <div class="content is-small" v-if="'lightInfo' in hero.t5">
+                <strong class="is-6">Notes</strong>
+                <br>
+                <span>{{ hero.t5.lightInfo }}</span>
+              </div>
               <div class="column content">
                 <label class="label no-margin-bottom">T5 Dark</label>
                 <span>{{ hero.t5.dark }}</span>
+              </div>
+              <div class="content is-small" v-if="'darkInfo' in hero.t5">
+                <strong class="is-6">Notes</strong>
+                <br>
+                <span>{{ hero.t5.darkInfo }}</span>
               </div>
             </div>
           </p>
