67eb3789da633e1db5784bee3ac2c4c4004e31ea
Bjorn Lim <mail@bjornl.im>
2018-03-09 00:16:54 +0800

    Regenerate yarn lock and fix visual bugs.

--- src/components/PageSelector.vue
+++ src/components/PageSelector.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="field" id="page-selector">
-    <div class="control is-expanded">
-      <div class="select is-fullwidth">
-        <select v-model="page" @change="pageChange">
-          <option value="hero">Hero</option>
-          <option value="artifact">Artifact</option>
-          <option value="calc">Calculator</option>
-        </select>
+  <div class="columns no-margin-bottom">
+    <div class="column">
+      <div class="field" id="page-selector">
+        <div class="control is-expanded">
+          <div class="select is-fullwidth">
+            <select v-model="page" @change="pageChange">
+              <option value="hero">Hero</option>
+              <option value="artifact">Artifact</option>
+              <option value="calc">Calculator</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
   </div>
